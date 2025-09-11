b3c2a6ff8d5f3737008e2fbaa166fa10a4ba465f
SAURAV YADAV <91652627+IamSaurav07@users.noreply.github.com>
2025-09-11 19:30:45 +0530

    Add files via upload

--- node-modules/@typescript-eslint/index.d.ts
+++ node-modules/@typescript-eslint/index.d.ts
@@ -1,13 +1,3 @@
-import type {
-  ClassicConfig,
-  FlatConfig,
-} from '@typescript-eslint/utils/ts-eslint';
-
-import type rules from './rules';
-
-declare const cjsExport: {
-  configs: Record<string, ClassicConfig.Config>;
-  meta: FlatConfig.PluginMeta;
-  rules: typeof rules;
-};
-export = cjsExport;
+import type { LibDefinition } from '../variable';
+export declare const lib: ReadonlyMap<string, LibDefinition>;
+//# sourceMappingURL=index.d.ts.map
